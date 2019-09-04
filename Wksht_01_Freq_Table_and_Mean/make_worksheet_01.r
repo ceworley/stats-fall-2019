493bea24755a2222991011e09b2e6764cb85540a
ceworley <ceworley@bhcc.mass.edu>
2019-09-04 06:41:23 -0400

    homework 1

--- Wksht_01_Freq_Table_and_Mean/make_worksheet_01.r
+++ Wksht_01_Freq_Table_and_Mean/make_worksheet_01.r
@@ -3,18 +3,15 @@ library("exams")
 
 
 q1 = "discrete_dotplot_unsorted.Rmd"
-q2 = "discrete_barplot_unsorted.Rmd"
-q3 = "continuous_hist_nearest_int.Rmd"
-q4 = "continuous_hist_nearest_mult5.Rmd"
-
-probs = c(q1, q2, q3, q4)
+q2 = "continuous_hist_nearest_int.Rmd"
+probs = c(q1, q2)
 
 
-set.seed(12)
+set.seed(123)
 exams2pdf(probs, 
-          n = 1,
-          name = "histograms",
+          n = 22,
+          name = "homework",
           dir = "output",
           resolution = 300,
-          template = "worksheet.tex",
+          template = "homework.tex",
 )
