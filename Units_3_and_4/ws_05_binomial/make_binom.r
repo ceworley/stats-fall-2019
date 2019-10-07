4a51b54264a85858b7a86fa636ac6c6c49da322d
ceworley <ceworley@bhcc.mass.edu>
2019-10-07 07:45:24 -0400

    binomiall

--- Units_3_and_4/ws_05_binomial/make_binom.r
+++ Units_3_and_4/ws_05_binomial/make_binom.r
@@ -1,16 +1,16 @@
 library("exams")
 
-qs = "probdist.Rmd"
+qs = "binomial.Rmd"
 
 set.seed(12345)
 exams2pdf(qs,
-          name = "probdist",
+          name = "binomial",
           nsamp = 8,
           dir = "wksht",
-          template = "templates/worksheet_sols_end.tex",     
+          template = "templates/worksheet_sols.tex",     
           header = list(
             Date = "2019-OCT-2",
             ID = "001",
-            Titl = "Mean and SD of Probability Distribution"
+            Titl = "Binomial Distribution"
           )
 )
