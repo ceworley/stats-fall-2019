81a0376d70b955d5265dcabe1a321274e1705332
ceworley <ceworley@bhcc.mass.edu>
2019-10-23 20:06:06 -0400

    ok

---
+++ Units_5_and_6/get_norm_areas/make_spin_D_better.r
@@ -0,0 +1,15 @@
+library("exams")
+
+qs = c(rep("spinner_D_ordered.Rmd",2),rep("spinner_D.Rmd",4))
+
+set.seed(145)
+exams2pdf(qs,
+          name = "Spinner_D_better",
+          dir = "wksht",
+          template = "templates/worksheet_sols.tex",     
+          header = list(
+            Date = "2019-OCT-2",
+            ID = "001",
+            Titl = "Standard Normal Spinner"
+          )
+)
