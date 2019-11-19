1ca9b5ca0119e4683e017143c26b6e25dba518c1
ceworley <ceworley@bhcc.mass.edu>
2019-11-19 07:24:15 -0500

    hyp testing

--- Units_7_and_8/hyp_test/make_HT_B.r
+++ Units_7_and_8/hyp_test/make_HT_B.r
@@ -1,15 +1,15 @@
 library("exams")
 
-set.seed(123)
+set.seed(19)
 qs = c(rep("p_single_test_right_guessing.Rmd",2),rep("mean_twotail_t.Rmd",2),rep("mean_twotail_t_raw.Rmd",2))
 
 exams2pdf(qs,
           name = "HTB",
           dir = "wksht",
-          template = "templates/worksheet_sols.tex",     
+          template = "templates/worksheet_sols_end.tex",     
           header = list(
             Date = "2019-OCT-2",
             ID = "001",
-            Titl = "Single-Sample Hypothesis Testing B"
+            Titl = "Single-Sample HT B"
           )
 )
