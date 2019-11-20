6b6e97f2994cf80e7e1742f08920fc9039d91716
ceworley <ceworley@bhcc.mass.edu>
2019-11-20 11:46:52 +0000

    hyp test

--- Units_7_and_8/hyp_test/make_HT_C.r
+++ Units_7_and_8/hyp_test/make_HT_C.r
@@ -1,8 +1,23 @@
 library("exams")
 
 set.seed(123)
-qs = c(rep("p_single_test_left_resistance.Rmd",2))
-
+qs = c("p_single_test_left.Rmd",
+       "p_single_test_left_resistance.Rmd",
+       "p_single_test_right.Rmd",
+       "p_single_test_right_guessing.Rmd",
+       "p_single_test_twotail.Rmd",
+       "p_single_test_twotail_coin.Rmd",
+       "mean_lefttail.Rmd",
+       "mean_lefttail_dice.Rmd",
+       "mean_righttail.Rmd",
+       "mean_righttail_dice.Rmd",
+       "mean_twotail.Rmd",
+       "mean_twotail_dice.Rmd",
+       "mean_twotail_t.Rmd",
+       "mean_twotail_t_raw.Rmd"
+       )
+  
+  
 exams2pdf(qs,
           name = "HTC",
           dir = "wksht",
@@ -10,6 +25,6 @@ exams2pdf(qs,
           header = list(
             Date = "2019-OCT-2",
             ID = "001",
-            Titl = "HTC"
+            Titl = "Hypothesis Testing"
           )
 )
